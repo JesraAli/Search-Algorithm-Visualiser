4d26d04e8c0530be4870a8984eb633df8f44c1d4
psyja11 <psyja11@nottingham.ac.uk>
2024-06-15 18:36:39 +0100

    Implement Ternary Search, fix binary search and use iterative approach

--- src/main/java/io/github/jesraali/Main.java
+++ src/main/java/io/github/jesraali/Main.java
@@ -3,20 +3,26 @@ package io.github.jesraali;
 import io.github.jesraali.algorithms.arrays.BinarySearch;
 import io.github.jesraali.algorithms.arrays.LinearSearch;
 import io.github.jesraali.algorithms.arrays.SentinelLinearSearch;
+import io.github.jesraali.algorithms.arrays.TernarySearch;
 
 public class Main {
     public static void main(String[] args) {
 
         int[] array = {1, 2, 5, 8, 2, 4, 6};
-        int searchValue = 10;
+        int[] sortedArray = {1, 2, 2, 4, 5, 6, 8};
 
-        BinarySearch binarySearch = new BinarySearch();
-        System.out.println(binarySearch.run(array,searchValue));
+        int searchValue = 1;
+//
+//        BinarySearch binarySearch = new BinarySearch();
+//        System.out.println(binarySearch.run(sortedArray,searchValue));
 
-        LinearSearch linearSearch = new LinearSearch(array,searchValue);
-        System.out.println(linearSearch.run());
+//        LinearSearch linearSearch = new LinearSearch(array,searchValue);
+//        System.out.println(linearSearch.run());
+//
+//        SentinelLinearSearch sentinelLinearSearch = new SentinelLinearSearch(array,searchValue);
+//        System.out.println(sentinelLinearSearch.run());
 
-        SentinelLinearSearch SentinelLinearSearch = new SentinelLinearSearch(array,searchValue);
-        System.out.println(SentinelLinearSearch.run());
+        TernarySearch ternarySearch = new TernarySearch();
+        System.out.println(ternarySearch.run(sortedArray,searchValue));
     }
 }
--- src/main/java/io/github/jesraali/algorithms/arrays/BinarySearch.java
+++ src/main/java/io/github/jesraali/algorithms/arrays/BinarySearch.java
@@ -1,8 +1,34 @@
 package io.github.jesraali.algorithms.arrays;
 
+import java.util.Arrays;
+
+
 public class BinarySearch {
 
 
+    public int run(int[] array, int searchValue) {
+
+        // Input Array MUST be sorted for Binary Search.
+
+        // Always working on the same array, whether thats iteratively or recursively -This is so we can retrieve the indexes in the array.
+        // The only thing we change is the low and high pointers - allows us to change different parts of search space.
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (array[mid] == searchValue) {
+                return mid;
+            }
+            if (array[mid] < searchValue) {
+                low = mid + 1;
+            }
+            if (array[mid] > searchValue) {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
 //    int[] array;
 //    int searchValue;
 
@@ -11,28 +37,43 @@ public class BinarySearch {
 //        this.searchValue = searchValue;
 //    }
 
-    public int run(int[] array, int searchValue) {
-
-        int midpoint = array.length / 2;
-        if(array[midpoint] == searchValue) {
-            return midpoint;
-        }
-        else if(array[midpoint] < searchValue) {
-            int[] tempArray = new int[midpoint+1];
-
-            // Copy original array from starting position midpoint in  tempArray and 0 in original array, and copy until length of original array
-            System.arraycopy(array,midpoint,tempArray,0,array.length);
-            return run(tempArray, searchValue);
-        }
-        else if(array[midpoint] > searchValue) {
-            int[] tempArray = new int[midpoint+1]; // Create temporary array of length midpoint+1
-
-            // Copy original array from starting position 0 in both tempArray and original array, and copy until midpoint
-            System.arraycopy(array,0,tempArray,0,midpoint);
-            return run(tempArray, searchValue);
-        }
-
-        return -1;
-    }
+//    public int run(int[] array, int searchValue) {
+//
+//        // Input Array MUST be sorted for Binary Search.
+//        int low = 0;
+//        int high = array.length - 1;
+//
+//        return binarySearch(array,searchValue, low, high);
+//    }
+//
+//    public int binarySearch(int[] array, int searchValue, int low, int high){
+//        System.out.println("high:"+ high +" low:"+ low);
+//        System.out.println(Arrays.toString(array));
+//        int midpoint = (high-low)/2;
+//        System.out.println("midpoint:"+ midpoint);
+//        if(array[midpoint] == searchValue) {
+//            return midpoint;
+//        }
+//        else if(array[midpoint] < searchValue) {
+//            low = midpoint+1;
+//
+//            int[] tempArray = new int[midpoint+1];
+//
+//            // Copy original array from starting position midpoint in  tempArray and 0 in original array, Length of elements to be copied is array.length;
+//            System.arraycopy(array,midpoint,tempArray,0,array.length-midpoint);
+//            return binarySearch(tempArray, searchValue,low,high);
+//        }
+//        else if(array[midpoint] > searchValue) {
+//            high = midpoint-1;
+//
+//            int[] tempArray = new int[midpoint+1]; // Create temporary array of length midpoint+1
+//
+//            // Copy original array from starting position 0 in both tempArray and original array. Length of elements to be copied is 'midpoint;
+//            System.arraycopy(array,0,tempArray,0,midpoint);
+//            return binarySearch(tempArray, searchValue,low,high);
+//        }
+//
+//        return -1;
+//    }
 
 }
