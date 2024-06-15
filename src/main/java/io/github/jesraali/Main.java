716405794de1cd4bb94c406d45d59b2922ddc2a4
JesraAli <jesra.ali@gmail.com>
2024-06-16 00:40:01 +0100

    Implement Jump Search algorithm

--- src/main/java/io/github/jesraali/Main.java
+++ src/main/java/io/github/jesraali/Main.java
@@ -1,9 +1,6 @@
 package io.github.jesraali;
 
-import io.github.jesraali.algorithms.arrays.BinarySearch;
-import io.github.jesraali.algorithms.arrays.LinearSearch;
-import io.github.jesraali.algorithms.arrays.SentinelLinearSearch;
-import io.github.jesraali.algorithms.arrays.TernarySearch;
+import io.github.jesraali.algorithms.arrays.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +8,7 @@ public class Main {
         int[] array = {1, 2, 5, 8, 2, 4, 6};
         int[] sortedArray = {1, 2, 2, 4, 5, 6, 8};
 
-        int searchValue = 1;
+        int searchValue = 8;
 //
 //        BinarySearch binarySearch = new BinarySearch();
 //        System.out.println(binarySearch.run(sortedArray,searchValue));
@@ -22,7 +19,10 @@ public class Main {
 //        SentinelLinearSearch sentinelLinearSearch = new SentinelLinearSearch(array,searchValue);
 //        System.out.println(sentinelLinearSearch.run());
 
-        TernarySearch ternarySearch = new TernarySearch();
-        System.out.println(ternarySearch.run(sortedArray,searchValue));
+//        TernarySearch ternarySearch = new TernarySearch();
+//        System.out.println(ternarySearch.run(sortedArray,searchValue));
+
+        JumpSearch jumpSearch = new JumpSearch(sortedArray,searchValue);
+        System.out.println(jumpSearch.run());
     }
 }
--- src/main/java/io/github/jesraali/algorithms/arrays/JumpSearch.java
+++ src/main/java/io/github/jesraali/algorithms/arrays/JumpSearch.java
@@ -1,4 +1,40 @@
 package io.github.jesraali.algorithms.arrays;
 
+/** Array must be already sorted
+ * checks fewer elements by jumping ahead a fixed amount of steps
+ * have a block size m, we continue jumping in this interval until we hit an index element value
+ * that is greater than the search value
+ * We then perform a LINEAR search from the jump index element that was not greater than the search value
+ *
+ * Is BETTER than linear search. Worse than binary search
+ *
+ * -> Best step size is root(n) - where n is the length of the array
+ * */
 public class JumpSearch {
+
+    int[] array;
+    int searchValue;
+
+    public JumpSearch(int[] array, int searchValue){
+        this.array = array;
+        this.searchValue = searchValue;
+    }
+
+    public int run() {
+
+        int arrayLength = array.length;
+        double jumpSize = Math.floor(Math.sqrt(arrayLength)); //Floor the square root value so it's not a long number
+        int i = 0;
+        while(array[i]< searchValue){
+            i+= (int) jumpSize;
+        }
+        i -= (int) jumpSize; // Revert it back to i value that is not > search value
+
+        for (int j = i; j < array.length; j++) {
+            if (array[j] == searchValue) {
+                return j;
+            }
+        }
+        return -1;
+    }
 }
