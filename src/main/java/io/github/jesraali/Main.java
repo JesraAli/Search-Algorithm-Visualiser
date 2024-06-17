a881269a7c084c1a9a6d18738ea1d92115b002e3
JesraAli <jesra.ali@gmail.com>
2024-06-18 00:08:52 +0100

    Add javafx module location to build.gradle

--- src/main/java/io/github/jesraali/Main.java
+++ src/main/java/io/github/jesraali/Main.java
@@ -1,14 +1,41 @@
 package io.github.jesraali;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 import io.github.jesraali.algorithms.arrays.*;
 
-public class Main {
+import java.io.IOException;
+
+public class Main extends Application {
+
+
+
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Screen1.fxml"));
+        AnchorPane rootLayout = fxmlLoader.load();
+
+        Scene scene = new Scene(rootLayout, 800, 600);
+        stage.setScene(scene);
+        stage.setTitle("Search Algorithm Visualiser");
+        stage.show();
+    }
+
+
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 5, 8, 2, 4, 6};
-        int[] sortedArray = {1, 2, 2, 4, 5, 6, 8};
 
-        int searchValue = 8;
+        launch(args);
+//        int[] array = {1, 2, 5, 8, 2, 4, 6};
+//        int[] sortedArray = {1, 2, 2, 4, 5, 6, 8};
+//
+//        int searchValue = 8;
 //
 //        BinarySearch binarySearch = new BinarySearch();
 //        System.out.println(binarySearch.run(sortedArray,searchValue));
@@ -22,7 +49,7 @@ public class Main {
 //        TernarySearch ternarySearch = new TernarySearch();
 //        System.out.println(ternarySearch.run(sortedArray,searchValue));
 
-        JumpSearch jumpSearch = new JumpSearch(sortedArray,searchValue);
-        System.out.println(jumpSearch.run());
+//        JumpSearch jumpSearch = new JumpSearch(sortedArray,searchValue);
+//        System.out.println(jumpSearch.run());
     }
 }
