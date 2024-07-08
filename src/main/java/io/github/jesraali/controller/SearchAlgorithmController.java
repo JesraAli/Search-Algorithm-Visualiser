aeeac49923eba2fcdf107e7eb6813ad71129909a
JesraAli <jesra.ali@gmail.com>
2024-07-08 18:07:01 +0100

    Add classpath to application.properties, fixed whitelabel error page

--- src/main/java/io/github/jesraali/controller/SearchAlgorithmController.java
+++ src/main/java/io/github/jesraali/controller/SearchAlgorithmController.java
@@ -3,21 +3,26 @@ package io.github.jesraali.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 /** Handles requests and returns names of FreeMarker templates */
 @Controller
 public class SearchAlgorithmController {
 
-    @GetMapping("/") //Maps HTTP GET requests to the root / to the home() method
-    public String home() {
+    @GetMapping(value = {"/", "/index"})
+    public String home(Model model) {
+//        List<String> algorithms = Arrays.asList("Binary Search", "Linear Search", "Jump Search", "Interpolation Search");
+//        model.addAttribute("algorithms", algorithms);
         return "index"; // Returns index.ftl from templates directory
     }
 
-    @GetMapping("/visualize") //Maps HTTP GET requests to /visualize. Takes in parameter of "algorithm"
-    public String visualize(@RequestParam("algorithm") String algorithm, Model model) {
-        model.addAttribute("algorithm", algorithm);
-        return "visualize"; // Adds the 'algorithm' to the 'Model', and returns visualize.ftl from templates directory
-    }
+//
+//    @GetMapping("/visualise") //Maps HTTP GET requests to /visualize. Takes in parameter of "algorithm"
+//    public String visualize(@RequestParam("algorithm") String algorithm, Model model) {
+//        model.addAttribute("algorithm", algorithm);
+//        return "visualise"; // Adds the 'algorithm' to the 'Model', and returns visualize.ftl from templates directory
+//    }
 }
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-// Freemarker Template Language
-
-<html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Search Algorithm Visualiser</title>
+    </head>
     <body>
-        <#include 'fragments/menu.ftl'>
-        <h6>Dashboard page</h6>
+    <h1>Welcome to the Search Algorithm Visualiser</h1>
+    <p>Select an algorithm to visualise:</p>
+<#--    <form action="/visualise" method="get">-->
+<#--        <select name="algorithm">-->
+<#--            <#list algorithms as algorithm>-->
+<#--                <option value="${algorithm}">${algorithm}</option>-->
+<#--            </#list>-->
+<#--        </select>-->
+<#--        <button type="submit">Visualise</button>-->
+<#--    </form>-->
     </body>
 </html>
